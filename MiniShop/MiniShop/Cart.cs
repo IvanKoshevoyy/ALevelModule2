0df1a79fca0cce5d1b1524b23729a1663fef3464
IvanKoshevoyy <ivankoshevoy2@gmail.com>
2021-11-30 16:26:40 +0200

    lecture2 homework

---
+++ MiniShop/MiniShop/Cart.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    class Cart
+    {
+        public List<Product> Goods { get; set; }
+        public Cart()
+        {
+            Goods = new List<Product>();
+        }
+        public void AddProductToCart(Product name, int Amount)
+        {
+            name.Amount = Amount;
+            Goods.Add(name);
+        }
+        public void RemouveproductFromCart(Product name)
+        {
+             Goods.Remove(name);
+        }
+        public void ChangeAmountProductInCart(Product name, int Amount)
+        {
+            name.Amount = Amount;
+            foreach (Product prod in Goods)
+            {
+                if (prod.Name == name.Name && prod.Price == name.Price && prod.Volume == name.Volume)
+                {
+                    Goods.Remove(name);
+                    Goods.Add(name);
+                    break;
+                }
+            }
+        }
+      
+        public int GetAmountProd()
+        {
+            return Goods.Count();
+        }
+    }
+}
---
+++ MiniShop/MiniShop/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    class Product
+    {
+        public string Name { get; set; }
+        private double price;
+        public double Price 
+        {
+            get => price;
+            set
+            {
+                if (value > 0)
+                    price = value;
+            }
+        }
+        private int amount;
+        public int Amount 
+        {
+            get => amount;
+            set
+            {
+                if (value >= 0)
+                    amount = value;
+            }
+        }
+        private double volume;
+        public double Volume 
+        {
+            get => volume;
+            set
+            {
+                if (value > 0)
+                    volume = value;
+            }
+        }
+
+        public Product(string Name, double Price, int Amount, double Volume)
+        {
+            this.Volume = Volume;
+            this.Amount = Amount;
+            this.Price = Price;
+            this.Name = Name;
+        }
+        public Product(string Name, double Price, double Volume)
+        {
+            this.Volume = Volume;
+            this.Price = Price;
+            this.Name = Name;
+        }
+
+    }
+}
